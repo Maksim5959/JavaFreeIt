88c72823990cbe00a45e4fe05e6b24bed654a392
MaksimChuyashkou <luisyonger@gmail.com>
2021-02-16 23:09:23 +0300

    lesson1: hello world

--- Lesson1/src/HelloWorld.java
+++ Lesson1/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World Java!");
         System.out.println("My name is Maksim Chuyashkou.");
     }
 }
