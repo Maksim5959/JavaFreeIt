33c63b815af4892a3bfcb0536511af9bdcffb1c9
MaksimChuyashkou <luisyonger@gmail.com>
2021-04-22 22:31:47 +0300

    corected

--- lesson5/src/Task22.java
+++ lesson5/src/Task22.java
@@ -1,22 +1,19 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /*
-* Написать программу, выполняющую поиск в строке шестнадцатеричных чисел,
-* записаных по правилам Java, с помощью регулярных выражений и вывести их на
-* страницу.
-*/
+ * Написать программу, выполняющую поиск в строке шестнадцатеричных чисел,
+ * записаных по правилам Java, с помощью регулярных выражений и вывести их на
+ * страницу.
+ */
 public class Task22 {
     public static void main(String[] args) {
-        String str = "Hello    0x12AF12.,! Java World .,=0x15AD120x65AA!! ...\'0x156654";
-        String [] strSplit = str.split("0[xX]");
+        String str = "Hello    0x12AF12.,! Java World .,=0x15AD12 0x65AA!! ...\'0x156654";
         System.out.println("Шестнадцатиричные числа в заданной строке: ");
-        for (int i = 0;i < strSplit.length;i++){
-            strSplit [i] = "0x" + strSplit [i];
-            Pattern pattern = Pattern.compile("0[xX][0-9a-fA-F]+");
-            Matcher matcher = pattern.matcher(strSplit [i]);
-            if (matcher.find()) {
-                System.out.println(strSplit [i].substring(matcher.start(),matcher.end()));
-            }
+        Pattern pattern = Pattern.compile("0[xX][0-9a-fA-F]+");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            System.out.println(str.substring(matcher.start(), matcher.end()));
         }
     }
 }
