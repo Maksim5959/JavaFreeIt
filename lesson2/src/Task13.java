d1ba61e253fd3b32840c037d32df8f6e568a127c
MaksimChuyashkou <luisyonger@gmail.com>
2021-04-22 21:29:25 +0300

    corected

--- lesson2/src/Task13.java
+++ lesson2/src/Task13.java
@@ -8,22 +8,17 @@ import java.util.Scanner;
 public class Task13 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите количество чисел: ");
-        int countNumbers = sc.nextInt();
-        int countNumbersMarker = countNumbers;
-        Random rand = new Random();
-        int [] numbers = new int[countNumbers];
-        for (countNumbers = 0; countNumbers < numbers.length; countNumbers++) {
-            System.out.println("Введите число, осталось ввести " + "<" + (countNumbersMarker--) + ">");
-            numbers[countNumbers] = sc.nextInt();
-        }
+        Random random = new Random();
+        int[] array = new int[500];
+
         int maxNumber = Integer.MIN_VALUE;
-        for (countNumbers = 0; countNumbers < numbers.length; countNumbers++) {
-            if (numbers[countNumbers] > maxNumber) {
-                maxNumber = numbers[countNumbers];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(11);
+            if (array[i] > maxNumber) {
+                maxNumber = array[i];
             }
         }
+
         System.out.println("Максимальное число = " + maxNumber);
     }
 }
