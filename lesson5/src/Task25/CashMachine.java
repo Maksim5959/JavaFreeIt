63d0d5f2bc7697798aeb0af521de985576ddb873
MaksimChuyashkou <luisyonger@gmail.com>
2021-04-25 00:22:30 +0300

    corected

--- lesson5/src/Task25/CashMachine.java
+++ lesson5/src/Task25/CashMachine.java
@@ -28,106 +28,52 @@ public class CashMachine {
     }
 
     public boolean withdrawMoney(int sumCash) {
-        int saveBill20 = countBill20, saveBill50 = countBill50, saveBill100 = countBill100;
-        int saveWithdraw50 = 0, saveWithdraw100 = 0;
-        int saveSumCash = sumCash;
-        int balance = countBill20 * 20 + countBill50 * 50 + countBill100 * 100;
-        if (sumCash > balance) {
-            System.out.println("В банкомате недостаточно средств.");
-            return false;
-        } else if (sumCash % 10 != 0 || sumCash <= 0 || sumCash < 20) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 20, 50, 100 BYN.");
-            return false;
-        } else if (countBill20 == 0 && countBill50 == 0 && (sumCash % 20 == 0 || sumCash % 100 != 0)) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 100 BYN.");
-            return false;
-        } else if (countBill20 == 0 && countBill100 == 0 && (sumCash % 20 == 0 || sumCash % 100 != 50)) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 50 BYN.");
-            return false;
-        } else if (countBill50 == 0 && countBill100 == 0 && (sumCash % 20 != 0 || sumCash % 20 == 10)) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 20 BYN.");
-            return false;
-        } else if (countBill20 == 0 && (sumCash % 20 == 0 || sumCash % 20 == 10) && sumCash % 100 != 0 && sumCash % 50 != 0) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 50, 100 BYN.");
-            return false;
-        } else if (countBill50 == 0 && (sumCash % 100 == 50 || sumCash % 20 != 0 )) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 20, 100 BYN.");
-            return false;
-        }
-        int withdraw100 = sumCash / 100;
-        for (; ; ) {
-            if (withdraw100 == 0 || countBill100 == 0) {
-                break;
-            } else if (withdraw100 > countBill100 && (sumCash - withdraw100 * 100 != 30 || sumCash - withdraw100 * 100 > 20)) {
-                saveWithdraw100 = countBill100;
-                sumCash -= countBill100 * 100;
-                countBill100 = 0;
-                break;
-            } else if ((sumCash - withdraw100 * 100 == 30 || sumCash - withdraw100 * 100 < 20) && sumCash - withdraw100 * 100 != 0) {
-                withdraw100--;
-            } else {
-                saveWithdraw100 = withdraw100;
-                sumCash -= withdraw100 * 100;
-                countBill100 -= withdraw100;
-                break;
+        boolean cashOut = false;
+        int withdraw100 = 0, withdraw50 = 0, withdraw20 = 0;
+
+        while (sumCash != 0) {
+
+            for (int i = countBill100; i > 0; i--) {
+                if (sumCash - (i * 100) == 0
+                        || (sumCash - (i * 100) > 0 && sumCash - (i * 100) >= 20
+                        && sumCash - (i * 100) != 30)) {
+                    sumCash -= i * 100;
+                    withdraw100 = i;
+                    cashOut = true;
+                    break;
+                }
             }
-        }
-        int varCountBill50 = 0;
-        int withdraw50 = sumCash / 50;
-        for (; ; ) {
-            if (withdraw50 == 0 || countBill50 == 0) {
-                break;
-            }else if ((sumCash - withdraw50 * 50 == 30 || sumCash - withdraw50 * 50 < 20
-                    || sumCash - withdraw50 * 50 == 70) && (sumCash - withdraw50 * 50 != 0)) {
-                withdraw50--;
-            } else if (withdraw50 > countBill50 && (sumCash - withdraw50 * 50 != 30 || sumCash - withdraw50 * 50 > 20
-                    || sumCash - withdraw50 * 50 != 70) && (sumCash - countBill50*50) % 20 != 0) {
-                withdraw50 = countBill50--;
-                varCountBill50++;
+            for (int i = countBill50; i > 0; i--) {
+                if (sumCash - (i * 50) == 0
+                        || (sumCash - (i * 50) > 0 && sumCash - (i * 50) >= 20
+                        && sumCash - (i * 50) != 30
+                        && (sumCash - (i * 50))  % 20 == 0))  {
+                    sumCash -= i * 50;
+                    withdraw50 = i;
+                    cashOut = true;
+                    break;
+                }
             }
-            else if (withdraw50 > countBill50 && (sumCash - withdraw50 * 50 != 30 || sumCash - withdraw50 * 50 > 20
-                    || sumCash - withdraw50 * 50 != 70)) {
-                saveWithdraw50 = countBill50;
-                sumCash -= countBill50 * 50;
-                countBill50 = 0;
+            for (int i = countBill20; i > 0; i--) {
+                if (sumCash % 20 == 0 && sumCash != 0 && sumCash - i * 20 == 0) {
+                    sumCash -= i * 20;
+                    withdraw20 = i;
+                    cashOut = true;
+                    break;
+                }
+            }
+
+            if (sumCash != 0) {
+                cashOut = false;
+                System.out.println("Не дам!");
                 break;
             } else {
-                saveWithdraw50 = withdraw50;
-                sumCash -= withdraw50 * 50;
-                countBill50 -= withdraw50;
-                break;
+                System.out.println("Выдано соток: " + withdraw100 + " Выдано пятихатов: " + withdraw50 + " Выдано дваценов " + withdraw20);
             }
         }
-        int withdraw20 = sumCash / 20;
-        if (withdraw20 > countBill20 || sumCash % 20 != 0) {
-            System.out.println("Некорректно введена сумма, в банкомате имеются номиналы 20, 50, 100 BYN.");
-            countBill20 = saveBill20;
-            countBill50 = saveBill50;
-            countBill100 = saveBill100;
-            sumCash = saveSumCash;
-            return false;
-        } else if (countBill50 == 0 && countBill100 == 0 && sumCash % 20 != 0) {
-            System.out.println("В банкомате недостаточно купюр.Введите другую сумму.");
-            countBill20 = saveBill20;
-            countBill50 = saveBill50;
-            countBill100 = saveBill100;
-            sumCash = saveSumCash;
-            return false;
-        }
-        else {
-            sumCash -= withdraw20 * 20;
-            countBill20 -= withdraw20;
-            countBill50 = countBill50 + varCountBill50;
-            if (saveWithdraw100 != 0) {
-                System.out.println("Выдано " + saveWithdraw100 + " купюр(ы) номиналом 100 BYN");
-            }
-            if (saveWithdraw50 != 0) {
-                System.out.println(("Выдано " + saveWithdraw50 + " купюр(ы) номиналом 50 BYN"));
-            }
-            if (withdraw20 != 0) {
-                System.out.println(("Выдано " + withdraw20 + " купюр(ы) номиналом 20 BYN"));
-            }
-            return true;
-        }
+        countBill100 -= withdraw100;
+        countBill50 -= withdraw50;
+        countBill20 -= withdraw20;
+        return cashOut;
     }
 }
--- lesson5/src/Task25/CashMachineRunner.java
+++ lesson5/src/Task25/CashMachineRunner.java
@@ -11,21 +11,28 @@ package Task25;
 
 public class CashMachineRunner {
     public static void main(String[] args) {
-        CashMachine belarusbank = new CashMachine(20, 20, 20);
+        CashMachine belarusbank = new CashMachine(20, 20,20 );
+
         belarusbank.addMoney20(1);
         belarusbank.addMoney50(1);
         belarusbank.addMoney100(1);
+
         System.out.println("Остаток средств в банкомате: " + belarusbank.checkBalance() + "\n");
+
         if (belarusbank.withdrawMoney(3570)) {
             System.out.println("Операция прошла успешно. Заберите ваши деньги.\n");
         }
+
         System.out.println("Остаток средств в банкомате: " + belarusbank.checkBalance() + "\n");
         belarusbank.addMoney20(10);
+
         System.out.println("Остаток средств в банкомате: " + belarusbank.checkBalance() + "\n");
         belarusbank.withdrawMoney(200);
+
         if (belarusbank.withdrawMoney(450)) {
             System.out.println("Операция прошла успешно. Заберите ваши деньги.\n");
         }
+
         System.out.println("Остаток средств в банкомате: " + belarusbank.checkBalance() + "\n");
     }
 }
